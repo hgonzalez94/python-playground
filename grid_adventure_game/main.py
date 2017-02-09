10d9421e95598e9dcd78e7996155f6c3a4164e8f
Hugo Gonzalez <hagonzalez94@gmail.com>
2017-02-09 14:07:45 -0800

    added conditional to item loop

--- grid_adventure_game/main.py
+++ grid_adventure_game/main.py
@@ -3,6 +3,8 @@ items = ["frostmourne", "doomhammer", "ashbringer", "ashes of a'lar", "lightsabe
 def read_items():
     for item in items:
         print "Found {0} in collection of items".format(item)
+        if item == "doomhammer":
+            print "BRUH WE OUT HERE ON THE ENHANCE!!!"
 
     print ""
     print "the first item is {0}, the second item is {1}".format(items[0], items[1])
