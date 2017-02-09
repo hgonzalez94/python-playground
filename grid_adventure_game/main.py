d74529f67f09fd43cb63f083578bab255e13555a
Hugo Gonzalez <hagonzalez94@gmail.com>
2017-02-09 14:37:05 -0800

    Added Game Board Print Logic

--- grid_adventure_game/main.py
+++ grid_adventure_game/main.py
@@ -1,5 +1,25 @@
 items = ["frostmourne", "doomhammer", "ashbringer", "ashes of a'lar", "lightsaber"]
 
+game_grid = [
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+                [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+            ]
+
+def print_game_grid():
+    print "GAME GRID"
+    for row in game_grid:
+        for column in row:
+            print column,
+        print ""
+
 def read_items():
     for item in items:
         print "Found {0} in collection of items".format(item)
@@ -10,3 +30,4 @@ def read_items():
     print "the first item is {0}, the second item is {1}".format(items[0], items[1])
 
 read_items()
+print_game_grid()
